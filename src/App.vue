8cc3da6728557cc706c09b5c307d0bce87341267
@hashaydar29 <hashaydar29@gmail.com>
2021-01-17 11:13:32 +0200

    just added the app to the git repository

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Header />
+  <router-view />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/layout/header'
 
 export default {
-  name: 'App',
+  name: "app",
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
@@ -19,8 +19,19 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="todo-item" v-bind:class="{'is-complete': todo.completed}">       <!-- is-complete css styling class will be rendered conditionally -->
+        <p>
+            <input type="checkbox" v-on:change="markComplete(todo)">
+            {{todo.title}}
+            <button @click="$emit('del-todo', todo.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "TodoItem",
+    props: ["todo"],
+    methods: {
+        markComplete(todo) { 
+            todo.completed = !todo.completed;
+        }                    
+    }
+} 
+</script> <!-- for the markComplete function we can acces the data through this.todo -->
+
+<style scoped>
+     .todo-item {
+    background: #f4f4f4;
+    padding: 10px;
+    border-bottom: 1px #ccc dotted;
+    }
+    .is-complete {
+        text-decoration: line-through;
+    }
+  .del {
+    background: #ff0000;
+    color: #fff;
+    border: none;
+    padding: 5px 9px;
+    border-radius: 50%;
+    cursor: pointer;
+    float: right;
+    }
+</style>
